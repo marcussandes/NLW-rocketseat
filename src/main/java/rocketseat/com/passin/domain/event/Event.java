709e2302f8d4503ae82a907542212a61eb634674
Marcus Sandes <marcus.sandesf@gmail.com>
2024-04-04 00:18:25 -0300

    Primeiro commit

---
+++ src/main/java/rocketseat/com/passin/domain/event/Event.java
@@ -0,0 +1,23 @@
+package rocketseat.com.passin.domain.event;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "events")
+public class Event {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String detail;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
+
+}
